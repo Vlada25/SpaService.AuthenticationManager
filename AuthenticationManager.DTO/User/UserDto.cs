ad689428a2062a8672c1058dfa339c605176fc2e
Vlada <aladka03@gmail.com>
2022-07-23 10:26:36 +0300

    code was cleared

--- AuthenticationManager.DTO/User/UserDto.cs
+++ AuthenticationManager.DTO/User/UserDto.cs
@@ -7,6 +7,5 @@
         public string MiddleName { get; set; }
         public string UserName { get; set; }
         public string Email { get; set; }
-        public string PhotoId { get; set; }
     }
 }
