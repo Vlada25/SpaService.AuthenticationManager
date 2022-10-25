3a70b8b8dfd868e823586cbd2db1aa26237f1279
Vlada <aladka03@gmail.com>
2022-10-25 14:31:15 +0300

    updated migration

--- AuthenticationManager.Database/Migrations/20221025113046_FakeUsers.Designer.cs
+++ AuthenticationManager.Database/Migrations/20221025113046_FakeUsers.Designer.cs
@@ -4,6 +4,7 @@ using AuthenticationManager.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AuthenticationManager.Database.Migrations
 {
     [DbContext(typeof(AuthManagerDbContext))]
-    partial class AuthManagerDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221025113046_FakeUsers")]
+    partial class FakeUsers
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
